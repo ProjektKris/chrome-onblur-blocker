0d47315adfc522c9355a5a57d59e355e7f36c8d2
ProjektKris <rbxkrisxyz@gmail.com>
2021-11-10 22:05:27 +0700

    deal with .addEventListener

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 var s = document.createElement("script");
 s.src = chrome.runtime.getURL("onblur_disconnect.js");
-s.onload = function () {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
+// s.onload = function () {
+//     this.remove();
+// };
+
+// deal with .onXYZ
+(document.head || document.documentElement).prepend(s);
+
+// deal with .addEventListener
+window.addEventListener(
+    "focus",
+    function (event) {
+        event.stopImmediatePropagation();
+    },
+    true
+);
+window.addEventListener(
+    "blur",
+    function (event) {
+        event.stopImmediatePropagation();
+    },
+    true
+);
